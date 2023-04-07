1d513e4f9cdff4b26028554535422d7c5bfed3bb
rferreira1984 <rferreira1984@gmail.com>
2023-04-07 15:09:40 -0300

    Add files via upload

---
+++ assets/js/api_treinamento.js
@@ -0,0 +1,34 @@
+
+
+  function busca_usuario() {
+    var dados = [];
+    dados.push(40);
+     dados.push(60);
+   return dados;
+  }
+  function busca_treinamento() {
+ var dados=[];
+    fetch('http://177.124.177.223:81/Usuario/Todos')
+    .then(response => response.json())
+.then(data => {
+  const quantidadeDados = data.length;
+  const paragrafo = document.querySelector('h4');
+  paragrafo.innerHTML = `${quantidadeDados}`;
+    let contadorZero = 0;
+    let contadorUm = 0;
+    for (let i = 0; i < data.length; i++) {
+          if (data[i].mao_utilizada === 0) {
+            contadorZero++;
+          } else if (data[i].mao_utilizada === 1) {
+            contadorUm++;
+          }
+        }
+
+       dados.push(contadorZero);
+        dados.push(contadorUm);
+        console.log(dados);
+})
+
+.catch(error => console.error(error));
+return dados;
+  }
